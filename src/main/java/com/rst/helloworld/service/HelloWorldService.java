24b5ed2060fd5846f9f5fffa9a82f2494fabbdaf
administrator <jenkinstest2241@gmail.com>
2023-01-20 10:36:29 +0530

    new repo

---
+++ src/main/java/com/rst/helloworld/service/HelloWorldService.java
@@ -0,0 +1,33 @@
+package com.rst.helloworld.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+@Service
+public class HelloWorldService {
+
+	private static final Logger logger = LoggerFactory.getLogger(HelloWorldService.class);
+
+	public String getDesc() {
+
+		logger.debug("getDesc() is executed!");
+
+		return " Example Maven + Jenkins + Docker Project";
+
+	}
+
+	public String getTitle(String name) {
+
+		logger.debug("getTitle() is executed! $name : {}", name);
+
+		if(StringUtils.isEmpty(name)){
+			return "HELLO ASHOK LEYLAND TEAM";
+		}else{
+			return "Hello " + name;
+		}
+		
+	}
+
+}
